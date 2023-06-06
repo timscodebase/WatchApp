e48e1215dea3f885cd22d9fefc36f7f691e429a9
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-06-05 23:40:26 -0700

    Initial Commit

---
+++ WatchApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchApp Watch App
+//
+//  Created by Timothy Smith on 6/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchApp Watch App/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp Watch App
+//
+//  Created by Timothy Smith on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
